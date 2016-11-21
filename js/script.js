d930cf37fbc32a93e833e679238b73dd13254dd7
Natalya Ivanova <natalyas.ivanova@gmail.com>
2016-11-21 21:43:58 +0200

    Create timer object

--- js/script.js
+++ js/script.js
@@ -1,16 +1,33 @@
+var start, stop = 0;
+
+const msInSec = 1000;
+const msInMin = 60000;
+const msInHour = 3600000;
+
+function Timer(ms) {
+    this.miliseconds = ms;
+    this.toString = function() {
+        var seconds, minutes, hours;
+        minutes = seconds = 0;
+        return twoDigits(minutes) + ":" + twoDigits(seconds) + ":" + twoDigits(ms);
+    }
+}
+
 var timerContainer = document.getElementsByClassName("timer")[0];
-
-var start = 0;
-var stop = 0;
-
-var miliseconds = 0;
-var seconds = 0;
-var minutes = 0;
-
-timerContainer.innerText = minutes + ":" + seconds + ":" + miliseconds;
-
 var startButton = document.getElementsByClassName("start-button")[0];
 var clearButton = document.getElementsByClassName("clear-button")[0];
+
+// miliseconds = 0;
+
+// miliseconds = toDigits(miliseconds);
+
+timer = new Timer(0);
+
+timerContainer.innerText = timer.toString();
+
+function twoDigits(num) {
+    return (num < 10) ? ("0" + num) : num;
+}
 
 function start() {
     alert("Start timer!");
