fcc545a5921646435963c329e5fc7e76cb61d35a
Natalya Ivanova <natalyas.ivanova@gmail.com>
2016-11-23 14:44:33 +0200

    Add Pause and Continue options

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 var timerOn;
+var inProgress = false;
 
-function Timer(time) {
+function Timer(time, container) {
     const msInSec = 1000;
     const msInMin = 60000;
     const msInHour = 3600000;
 
     this.timePassed = time;
+    this.container = container;
 
     this.toString = function() {
         var seconds, minutes, hours;
@@ -20,37 +22,68 @@ function Timer(time) {
          ":" + formatDigits(miliseconds, 3);
     }
 
-    this.draw = function(container) {
+    this.draw = function() {
         clock = this.toString().slice(0, -4)
             + "<span id='miliseconds'>"
             + timer.toString().slice(-3)
             + "</span>";
-        container.innerHTML = clock;
+        this.container.innerHTML = clock;
     }
 }
 
-var timerContainer = timer;
+var timerContainer = myTimer;
 var startButton = document.getElementById("start");
 var clearButton = document.getElementById("clear");
 
-timer = new Timer(0);
+timer = new Timer(0, timerContainer);
 
-timer.draw(timerContainer);
+timer.draw();
 
 function formatDigits(num, base) {
     return ("00" + num).slice(-base);
 }
 
+var current = 0;
+
 function startTimer() {
-    var start = Date.now();
-    timerOn = setInterval(function() {
-        timer.timePassed = Date.now() - start;
-        timer.draw(timerContainer);
-    }, 1);
+    if (inProgress) {
+        clearInterval(timerOn);
+        startButton.innerText = "continue";
+        // timer.draw();
+        inProgress = false;
+        console.log("paused:" + timer.timePassed);
+    }
+    else {
+        startButton.innerText = "pause";
+        var start = Date.now();
+        // console.log("before entering setInterval, start: " + start);
+        timerOn = setInterval(function() {
+            // console.log("Inside setInterval:");
+            // console.log("date: ", Date.now());
+            // console.log("start: " + start);
+            // console.log("time passed: " + timer.timePassed);
+            // console.log("Date.now() - start: " + timer.timePassed);
+            timer.timePassed = Date.now() + current - start;
+            timer.draw();
+            // if (timer.timePassed == 0) {
+            //     console.log("catch!");
+            // }
+            // setTimeout(function() {
+            //     clearInterval(timerOn);
+            // }, 100);
+        }, 1);
+        inProgress = true;
+        console.log("restarted:" + timer.timePassed);
+        current = timer.timePassed;
+    }
 }
 
 function clearTimer() {
     clearInterval(timerOn);
+    timer.timePassed = 0;
+    timer.draw();
+    startButton.innerText = "start";
+    inProgress = false;
 }
 
 startButton.addEventListener("click", startTimer, false);
